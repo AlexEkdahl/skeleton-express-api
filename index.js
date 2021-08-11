c74ad56acf9b2f5a323235ee92eb405e37258936
Alexander Ekdahl <77007088+AlexEkdahl@users.noreply.github.com>
2021-08-11 12:41:17 +0200

    fix

--- index.js
+++ index.js
@@ -23,9 +23,6 @@ runCommand('git', ['clone', repoURL, name])
     })
   })
   .then(() => {
-    return runCommand('cd', [name])
-  })
-  .then(() => {
     console.log('Done! 🏁')
     console.log('')
     console.log('To get started:')
